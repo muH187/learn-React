4e4d8fe6116abd161579de38f3093eca85d32f34
Muhammad Ali <proqualitythings100@gmail.com>
2024-07-31 20:34:02 +0500

    fetch data from an api and display it

---
+++ Display The Cards/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Terms And Conditions</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
