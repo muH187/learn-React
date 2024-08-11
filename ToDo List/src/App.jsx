4ef414dce145e6011ce1ecf2338b1a04336ef858
Muhammad Ali <proqualitythings100@gmail.com>
2024-08-11 15:20:02 +0500

    todo completed, now to add local storage

--- ToDo List/src/App.jsx
+++ ToDo List/src/App.jsx
@@ -1,24 +1,44 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import { v4 as uuidv4 } from 'uuid';
+
 
 function App() {
   const [todo, setTodo] = useState("")
   const [todos, setTodos] = useState([])
 
-  const handleEdit = () => {
-
+  const handleEdit = (e, id) => {
+    let todo = todos.filter(i=>i.id === id)
+    setTodo(todo[0].todo)
+    let newTodos = todos.filter(item=>{
+      return item.id!==id
+    })
+    setTodos(newTodos)
   }
-  const handleDelete = () => {
-    
+  const handleDelete = (e, id) => {
+    let newTodos = todos.filter(item=>{
+      return item.id!==id
+    })
+    setTodos(newTodos)
   }
   const handleAdd = () => {
-    setTodos([...todos, {todo, isCompleted: false}])
+    setTodos([...todos, {id: uuidv4(), todo, isCompleted: false}])
     setTodo("")
   }
   const handleChange = (e) => {
     setTodo(e.target.value)
    
   }
+  const handleCheckBox = (e) => {
+    let id = e.target.name
+    let index = todos.findIndex(item=>{
+      return item.id === id
+    })
+    let newTodos = [...todos]
+    newTodos[index].isCompleted = !newTodos[index].isCompleted
+    setTodos(newTodos)
+  }
+  
 
   return (
     <>
@@ -27,19 +47,22 @@ function App() {
         <div className="addTodo my-5">
           <h2 className='text-xl font-bold'>Add Todos</h2>
           <input onChange={handleChange} value={todo} type="text" className='w-1/2 text-lg p-3 rounded-md'/>
-          <button className='bg-violet-700 hover:bg-violet-800 rounded-lg px-4 py-1 text-white mx-5 font-bold text-xl' onClick={handleAdd}>Add</button>
+          <button className='bg-violet-700 hover:bg-violet-800 rounded-lg px-4 py-1 text-white mx-5 font-bold text-xl' onClick={handleAdd}>Save</button>
         </div>
         <h2 className='text-xl font-bold'>Your Todos</h2>
         <div className="todos">
+          {todos.length === 0 && <div className='m-5 font-semibold'>No Todos to display</div> }
           {todos.map(item=> {
 
           
-          return <div key={todo} className="todo flex w-1/3 justify-between my-3">
-            <input type="checkbox" value={todo.isCompleted} name='' id=''/>
-            <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
+          return <div key={item.id} className="todo flex w-1/3 justify-between my-3">
+            <div className='flex gap-5'>
+              <input name={item.id} onChange={handleCheckBox} type="checkbox" value={item.isCompleted} id=''/>
+              <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
+            </div>
             <div className="buttons">
-              <button className='bg-violet-700 hover:bg-violet-800 rounded-lg px-4 py-1 text-white mx-2 font-bold text-sm' onClick={handleEdit}>Edit</button>
-              <button className='bg-violet-700 hover:bg-violet-800 rounded-lg px-4 py-1 text-white mx-2 font-bold text-sm' onClick={handleDelete}>Delete</button>
+              <button className='bg-violet-700 hover:bg-violet-800 rounded-lg px-4 py-1 text-white mx-2 font-bold text-sm' onClick={(e)=>{handleEdit(e, item.id)}}>Edit</button>
+              <button className='bg-violet-700 hover:bg-violet-800 rounded-lg px-4 py-1 text-white mx-2 font-bold text-sm' onClick={(e)=>{handleDelete(e, item.id)}}>Delete</button>
             </div>
           </div>
           })}
