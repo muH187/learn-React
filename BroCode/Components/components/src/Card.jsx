bc9cf26142c0f810f8abfec8bc965905dcc89b0f
Muhammad Ali <proqualitythings100@gmail.com>
2024-08-28 22:03:42 +0500

    components

--- BroCode/Components/components/src/Card.jsx
+++ BroCode/Components/components/src/Card.jsx
@@ -2,9 +2,9 @@
 function Card() {
     return(
         <div className="card">
-            <img src="https://shorturl.at/IrEti" alt="Ali's Picture" />
-            <h2>Muhammad Ali</h2>
-            <p>He is an entreprenuer and businessmen with networth of 12 Million Dollars.</p>
+            <img className="card-image" src="https://shorturl.at/IrEti" alt="Ali's Picture" />
+            <h2 className="card-title">Muhammad Ali</h2>
+            <p className="card-text">He is an entreprenuer and businessmen with networth of 12 Million Dollars.</p>
         </div>
     )
 }
