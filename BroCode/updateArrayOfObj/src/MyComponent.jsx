7b446116ed35374e4f5a4106db8b51341feb75dd
Muhammad Ali <proqualitythings100@gmail.com>
2024-08-31 10:10:03 +0500

    udpate array obj

---
+++ BroCode/updateArrayOfObj/src/MyComponent.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+
+function MyComponent() {
+
+    const [cars, setCars] = useState([])
+    const [carYear, setCarYear] = useState(new Date().getFullYear())
+    const [carMake, setCarMake] = useState("")
+    const [carModel, setCarModel] = useState("")
+
+
+    function handleAddCar() {
+        const newCar = {year: carYear, make: carMake, model: carModel}
+
+        setCars(c => [...c, newCar])
+
+        setCarYear(new Date().getFullYear())
+        setCarMake("")
+        setCarModel("")
+    }
+
+    function handleRemoveCar(index) {
+        setCars(c => c.filter((_, i) => i !== index))
+    }
+
+    function handleYearChange(event) {
+        setCarYear(event.target.value)
+    }
+
+    function handleMakeChange(event) {
+        setCarMake(event.target.value)
+    }
+
+    function handleModelChange(event) {
+        setCarModel(event.target.value)
+    }
+
+    return (
+        <div>
+            <h1>List of car objects.</h1>
+            <ul>
+                {cars.map((car, index) => <li onClick={ () => handleRemoveCar(index)} key={index}> {car.year} {car.make} {car.model} </li>)}
+            </ul>
+
+            <input type="number" value={carYear} onChange={handleYearChange}/><br />
+            <input type="text" value={carMake} placeholder='Enter car make' onChange={handleMakeChange}/><br />
+            <input type="text" value={carModel} placeholder='Enter car model' onChange={handleModelChange}/><br />
+            <button onClick={handleAddCar}>Add Car</button>
+
+
+        </div>
+    )
+}
+
+export default MyComponent
