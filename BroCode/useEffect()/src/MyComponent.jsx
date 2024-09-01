5dd04c47d63399f7d14e14eaecc0cc8f3d92a217
Muhammad Ali <proqualitythings100@gmail.com>
2024-09-01 09:38:01 +0500

    useEffect()

---
+++ BroCode/useEffect()/src/MyComponent.jsx
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react'
+
+function MyComponent() {
+
+    const [count, setCount] = useState(0)
+    const [color, setColor] = useState("green")
+
+    useEffect(() => {
+        document.title = `Count: ${count} ${color}`
+    }, [count, color])
+
+    const addCount = () => {
+        setCount(c => c + 1)
+    }
+
+    const subtractCount = () => {
+        setCount(c => c - 1)
+    }
+
+    const changeColor = () => {
+        setColor(c => c === "green" ? "red" : "green")
+    }
+
+    return (
+        <>
+        <p style={{color: color}}>Count: {count}</p>
+        <button onClick={addCount}>Add</button>
+        <button onClick={subtractCount}>Subtract</button><br />
+        <button onClick={changeColor}>Change Color</button>
+        </>
+    )
+}
+
+export default MyComponent
